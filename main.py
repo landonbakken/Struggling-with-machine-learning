dcb4872a0302ced49dacced06ba1fc48e65b81b5
TheRad1 <18326@verona.k12.wi.us>
2023-10-01 12:57:51 -0500

    trying a different trainer

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ from gym import spaces
 import numpy as np
 import custom_env
 
+# pip install:
+# gym==0.25.2
+# numpy
+# tensorflow
+# keras-rl2
+
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Flatten
 from tensorflow.keras.optimizers.legacy import Adam
@@ -21,10 +27,15 @@ gym.register(
     entry_point="custom_env:GridWorldEnv",
 )
 
-env = gym.make("GridWorldEnv-v0")
+env = gym.make("GridWorldEnv-v0")  # CartPole-v1")
 
-states = 4  # env.observation_space.shape[0]
-actions = 2  # env.action_space.n
+states = env.observation_space.spaces["agent"].shape[
+    0
+]  # env.observation_space.shape[0]
+actions = env.action_space.n  # env.action_space.n
+
+print(states)
+print(actions)
 
 model = Sequential()
 model.add(Flatten(input_shape=(1, states)))
@@ -42,28 +53,9 @@ agent = DQNAgent(
 )
 
 agent.compile(Adam(lr=0.001), metrics=["mae"])
-agent.fit(env, nb_steps=100000, visualize=False, verbose=1)
+agent.fit(env, nb_steps=100000, visualize=True, verbose=1)
 
 results = agent.test(env, nb_episodes=10, visualize=True)
 print(np.mean(results.history["episode_reward"]))
 
 env.close()
-
-"""episodes = 10
-
-for episode in range(1, episodes + 1):
-    state = env.reset()
-    done = False
-    score = 0
-
-    while not done:
-        action = random.choice([0, 1])
-        _, reward, done, _ = env.step(action)
-        score += reward
-        env.render
-
-    print(f"Episode: {episode}, Score: {score}")
-
-
-env.close()
-"""
