049f07c542dc378ce51edaa2031e543ea5771bdf
TheRad1 <18326@verona.k12.wi.us>
2023-10-02 12:43:59 -0500

    found source code for lunar lander

--- main.py
+++ main.py
@@ -11,8 +11,12 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 
-# set of environment
-env = gym.make("LunarLander-v2")
+# set up environment
+import custom_env
+
+gym.register(id="TestEnv-v0", entry_point="custom_env:LunarLander")
+
+env = gym.make("TestEnv-v0")  # LunarLander-v2")
 
 # set up matplotlib
 is_ipython = "inline" in matplotlib.get_backend()
@@ -186,15 +190,11 @@ def optimize_model():
     optimizer.step()
 
 
-if torch.cuda.is_available():
-    num_episodes = 25
-else:
-    num_episodes = 25
-
-for i_episode in range(num_episodes):
+def PlayGame():
     # Initialize the environment and get it's state
     state, info = env.reset()
     state = torch.tensor(state, dtype=torch.float32, device=device).unsqueeze(0)
+
     for t in count():
         action = select_action(state)
         observation, reward, terminated, truncated, _ = env.step(action.item())
@@ -232,46 +232,21 @@ for i_episode in range(num_episodes):
             plot_durations()
             break
 
-# display result
+
+if torch.cuda.is_available():
+    num_episodes = 25
+else:
+    num_episodes = 25
+num_display = 3
+
+# train model
+for i_episode in range(num_episodes):
+    PlayGame()
+
+# make enviroment rendered and show result
 env = gym.make("LunarLander-v2", render_mode="human")
-state, info = env.reset()
-state = torch.tensor(state, dtype=torch.float32, device=device).unsqueeze(0)
-for t in count():
-    action = select_action(state)
-    observation, reward, terminated, truncated, _ = env.step(action.item())
-    reward = torch.tensor([reward], device=device)
-    done = terminated or truncated
-
-    if terminated:
-        next_state = None
-    else:
-        next_state = torch.tensor(
-            observation, dtype=torch.float32, device=device
-        ).unsqueeze(0)
-
-    # Store the transition in memory
-    memory.push(state, action, next_state, reward)
-
-    # Move to the next state
-    state = next_state
-
-    # Perform one step of the optimization (on the policy network)
-    optimize_model()
-
-    # Soft update of the target network's weights
-    # θ′ ← τ θ + (1 −τ )θ′
-    target_net_state_dict = target_net.state_dict()
-    policy_net_state_dict = policy_net.state_dict()
-    for key in policy_net_state_dict:
-        target_net_state_dict[key] = policy_net_state_dict[
-            key
-        ] * TAU + target_net_state_dict[key] * (1 - TAU)
-    target_net.load_state_dict(target_net_state_dict)
-
-    if done:
-        episode_durations.append(t + 1)
-        plot_durations()
-        break
+for i_result in range(num_display):
+    PlayGame()
 
 
 print("Complete")
