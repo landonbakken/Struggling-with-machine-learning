9fa254eb6d147d25fb0558669b88baac0a277593
TheRad1 <18326@verona.k12.wi.us>
2023-10-03 21:05:05 -0500

    a lot
    - cleaned up naming
    - got rid of DQN since its kinda bad
    - trained a lunar lander env to 1m steps

--- PPO_trainer.py
+++ PPO_trainer.py
@@ -4,15 +4,15 @@ from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
 
 # Parallel environments
-vec_env = make_vec_env("LunarLander-v2", seed=1, n_envs=4)
+vec_env = make_vec_env("BipedalWalker-v3", seed=1, n_envs=4)
 
-model = PPO("MlpPolicy", vec_env, verbose=1)
-model.learn(total_timesteps=200000)
-model.save("ppo_cartpole")
+# model = PPO.load("lunar_ppo_1000000.zip", env=vec_env)
+model = PPO("MlpPolicy", vec_env, verbose=1, device="auto")
+model.learn(total_timesteps=1000000)
+model.save("walker_ppo_1000000")
 
-del model  # remove to demonstrate saving and loading
+# del model  # remove to demonstrate saving and loading
 
-model = PPO.load("ppo_cartpole")
 
 obs = vec_env.reset()
 while True:
