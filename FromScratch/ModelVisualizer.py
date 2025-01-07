a79643781b8c635067dd005b1dd90c9068485373
TheRad1 <landon.bakken@gmail.com>
2025-01-06 19:23:28 -0600

    model visualizer mostly working

--- FromScratch/ModelVisualizer.py
+++ FromScratch/ModelVisualizer.py
@@ -1,5 +1,6 @@
 import pygame
 import VariableSliders
+import random
 
 #model settings
 dimentions = [2, 3, 2]
@@ -16,8 +17,7 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
 #visual settings
-NODE_COLOR_MARGIN = 30
-CONNECTION_COLOR = BLUE
+COLOR_MARGIN = 30
 NODE_SCALE = 1
 CONNECTION_WIDTH = 5
 NODE_RADIUS = 25
@@ -41,35 +41,64 @@ pygame.display.set_caption("Model Visualizer")
 class Model:
     def __init__(self, dimentions):
         self.layers = []
-        self.nodes = [] #just for drawing
 
-        pastLayerNodes = []
+        #create nodes
         for layerIndex in range(len(dimentions)):
             layerNodes = []
             for nodeIndex in range(dimentions[layerIndex]):
-                newNode = Node(layerIndex, nodeIndex, len(dimentions), dimentions[layerIndex], pastLayerNodes)
+                newNode = Node(layerIndex, nodeIndex, len(dimentions), dimentions[layerIndex])
                 layerNodes.append(newNode)
-                self.nodes.append(newNode)
-            pastLayerNodes = layerNodes
             self.layers.append(layerNodes)
+        
+        #connect nodes (not last layer since they don't have any)
+        for layerIndex in range(len(dimentions) - 1):
+            layer = self.layers[layerIndex]
+            nextLayer = self.layers[layerIndex + 1]
 
-        #for visual reasons (overlapping)
-        self.nodes.reverse()
+            for node in layer:
+                node.makeConnections(nextLayer)
     
     def draw(self):
-        #draw each node
-        for node in self.nodes:
-            node.draw()
+        #loop through layer
+        for layer in self.layers:
+            #get the max value per layer(for percent based coloring)
+            maxValue = max([node.value for node in layer])
+            maxValue = 1 if maxValue == 0 else maxValue #cant be 0
+
+            #get the max weight per layer (for percent based coloring)
+            if layer != self.layers[-1]:
+                maxWeight = max(layer[0].weights)
+                for node in layer:
+                    maxWeight = max(maxWeight, max(node.weights))
+
+                maxWeight = 1 if maxWeight == 0 else maxWeight #cant be 0
+            else:
+                maxWeight = 1 #doesnt matter since there arent connections for last layer
+
+            #draw each node
+            for node in layer:
+                node.draw(maxValue, maxWeight)
     
     def recalculate(self):
-        pass
+        #clear values (except for the first one)
+        for layerIndex in range(len(self.layers) - 1):
+            for node in self.layers[layerIndex + 1]:
+                node.clear()
+
+        #add values
+        for layer in self.layers:
+            for node in layer:
+                node.calculate()
+
+def clamp(value, minValue, maxValue):
+    return max(min(value, maxValue), minValue)
 
 class Node:
-    def __init__(self, layerIndex, nodeIndex, totalLayers, totalNodesInLayer, connectedNodes):
+    def __init__(self, layerIndex, nodeIndex, totalLayers, totalNodesInLayer):
         self.layerIndex = layerIndex
         self.nodeIndex = nodeIndex
-        self.connectedNodes = connectedNodes
-        self.weights = [] #corrispond with connectedNodes
+        self.connectedNodes = []
+        self.weights = []
         self.value = 0
 
         #get draw position
@@ -77,21 +106,65 @@ class Node:
         y = (int) (NODE_WINDOW_HEIGHT / 2 + (totalNodesInLayer / 2 - (nodeIndex + .5)) * NODE_SPACING)
         self.drawPosition = (x, y)
 
-    def draw(self):
+    def draw(self, maxValueInLayer, maxWeightInLayer):
         #connections (drawn first so they don't overlap nodes)
-        for connectedNode in self.connectedNodes:
-            color = 255
-            pygame.draw.line(screen, (color, color, color), self.drawPosition, connectedNode.drawPosition, CONNECTION_WIDTH)
+        for connectedNodeIndex in range(len(self.connectedNodes)):
+            color = (int) (255 * self.weights[connectedNodeIndex] / maxWeightInLayer)
+            if color > 255 or color < 0:
+                print(f"Connection color error: {self.weights[connectedNodeIndex]} -> {color}")
+                color = clamp(color, 0, 255)
+            pygame.draw.line(screen, (color, color, color), self.drawPosition, self.connectedNodes[connectedNodeIndex].drawPosition, CONNECTION_WIDTH)
         
         #the circle
-        color = (255 - NODE_COLOR_MARGIN) * self.value + NODE_COLOR_MARGIN / 2
+        color = (int) (255 * self.value / maxValueInLayer)
+        if color > 255 or color < 0:
+            print(f"Node color error: {self.value} -> {color}")
+            color = clamp(color, 0, 255)
         pygame.draw.circle(screen, (color, color, color), self.drawPosition, NODE_RADIUS)
+
+    #one node
+    def makeConnection(self, nodeToConnect):
+        self.connectedNodes.append(nodeToConnect)
+        self.weights.append(.5)
+
+    #a list of nodes
+    def makeConnections(self, nodesToConnect):
+        for node in nodesToConnect:
+            self.makeConnection(node)
+
+    def clear(self):
+        self.value = 0
+
+    def calculate(self):
+        #add value * weight to connected nodes
+        for connectedNodeIndex in range(len(self.connectedNodes)):
+            self.connectedNodes[connectedNodeIndex].value += self.value * self.weights[connectedNodeIndex]
 
 def updateSliderValues(sliderValues):
     for key, value in sliderValues.items():
+        #parse key into list
         parsedKey = [int(item.strip()) for item in key.split(",")]
-        model.layers[parsedKey[0]][len(model.layers[parsedKey[0]]) - parsedKey[1] - 1].value = value
+        
+        #get node
+        node = model.layers[parsedKey[0]][len(model.layers[parsedKey[0]]) - parsedKey[1] - 1]
+        
+        #if input
+        if len(parsedKey) == 2:
+            node.value = value
+        #if weight
+        else:
+            node.weights[parsedKey[2]] = value
+
     model.recalculate()
+    
+    # Fill the screen with white
+    screen.fill(BLACK)
+
+    #draw everything
+    model.draw() 
+
+    # Update the display
+    pygame.display.flip()
 
 def getSliderConfigFromDim(dimentions):
     #variable sliders
@@ -100,13 +173,16 @@ def getSliderConfigFromDim(dimentions):
     for dimentionIndex in range(len(dimentions) - 1):
         for valueIndex in range(dimentions[dimentionIndex]):
             if dimentionIndex == 0:
-                name = "Input"
-            elif dimentionIndex < len(dimentions) - 1:
-                name = "Hidden"
-            
-            id = f"{dimentionIndex}, {valueIndex}"
-            sliderConfig.append((f"{name} {valueIndex} Layer {dimentionIndex}", id, 0, 1))
-            sliderValues[id] = .5
+                id = f"{dimentionIndex}, {valueIndex}"
+                name = f"Input {valueIndex}"
+                sliderConfig.append((name, id, 0, 1))
+                sliderValues[id] = random.random()
+
+            for weight in range(dimentions[dimentionIndex + 1]):
+                id = f"{dimentionIndex}, {valueIndex}, {weight}"
+                name = f"Weight: layer {dimentionIndex}, node {valueIndex} -> {weight}"
+                sliderConfig.append((name, id, 0, 1))
+                sliderValues[id] = random.random()
 
     return sliderConfig, sliderValues
 
@@ -124,13 +200,7 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    # Fill the screen with white
-    screen.fill(BLACK)
-
-    model.draw()
-
-    # Update the display
-    pygame.display.flip()
+    #nothing currently
 
 # Quit Pygame
 pygame.quit()
